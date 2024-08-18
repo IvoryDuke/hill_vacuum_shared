ae18932f372c8f3aee188a3a471c7bd204a5bfc9
IvoryDuke <ivory.duke@skiff.com>
2024-08-18 14:18:28 +0200

    process_manual function

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //
 //=======================================================================//
 
-use std::ops::RangeInclusive;
+use std::{ops::RangeInclusive, path::{Path, PathBuf}};
 
 //=======================================================================//
 // CONSTANTS
@@ -194,4 +194,115 @@ macro_rules! match_or_panic {
             _ => panic!("Pattern does not match.")
         }
     };
+}
+
+//=======================================================================//
+// TYPES
+//
+//=======================================================================//
+
+pub enum ManualItem
+{
+    Regular,
+    Tool,
+    Texture
+}
+
+//=======================================================================//
+// FUNCTIONS
+//
+//=======================================================================//
+
+#[allow(clippy::missing_panics_doc)]
+#[inline]
+pub fn process_manual<
+    S: FnMut(&mut String, bool),
+    N: FnMut(&mut String, &str, ManualItem),
+    P: Fn(&mut String, &str, String, ManualItem),
+    E: FnMut(&mut String)
+>(
+    start_string: &str,
+    mut section_start: S,
+    mut section_name: N,
+    process_file: P,
+    mut section_end: E
+) -> String
+{
+    impl From<char> for ManualItem
+    {
+        #[inline]
+        fn from(value: char) -> Self
+        {
+            if value == 'S' || value == 'T'
+            {
+                Self::Tool
+            }
+            else if value == 'X'
+            {
+                Self::Texture
+            }
+            else
+            {
+                Self::Regular
+            }
+        }
+    }
+
+    #[inline]
+    fn stem_chars(path: &Path) -> (impl Iterator<Item = char> + '_, ManualItem)
+    {
+        let mut chars = path.file_stem().unwrap().to_str().unwrap().chars();
+        let first = chars.next_value();
+        (chars.skip_while(|c| !c.is_alphabetic()), first.into())
+    }
+
+    let mut string = start_string.to_owned();
+    let mut dirs = std::fs::read_dir(PathBuf::from("docs/manual/"))
+        .unwrap()
+        .map(|entry| entry.unwrap().path())
+        .collect::<Vec<_>>();
+    dirs.sort_unstable();
+    let last_index = dirs.len() - 1;
+
+    for (i, dir) in dirs.into_iter().enumerate()
+    {
+        section_start(&mut string, i == last_index);
+
+        let (mut chars, item) = stem_chars(&dir);
+        let mut name = String::from(chars.next_value().to_ascii_uppercase());
+
+        while let Some(mut c) = chars.by_ref().next()
+        {
+            if c == '_'
+            {
+                c = ' ';
+            }
+
+            name.push(c);
+        }
+
+        section_name(&mut string, &name, item);
+
+        let mut paths = std::fs::read_dir(&dir)
+            .unwrap()
+            .map(|entry| entry.unwrap().path())
+            .collect::<Vec<_>>();
+        paths.sort_unstable();
+
+        for path in paths
+        {
+            let (chars, item) = stem_chars(&path);
+
+            process_file(
+                &mut string,
+                &chars.collect::<String>(),
+                std::fs::read_to_string(&path).unwrap(),
+                item
+            );
+        }
+
+        section_end(&mut string);
+    }
+
+    string
 }
